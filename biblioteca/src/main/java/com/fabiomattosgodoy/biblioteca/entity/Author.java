b9f7973d4c2103ad634ad4640cd48ff7570fd11d
Fabio <fabiomdg1@gmail.com>
2022-06-16 16:37:55 -0300

    Criado entity Author

---
+++ biblioteca/src/main/java/com/fabiomattosgodoy/biblioteca/entity/Author.java
@@ -0,0 +1,17 @@
+package com.fabiomattosgodoy.biblioteca.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
