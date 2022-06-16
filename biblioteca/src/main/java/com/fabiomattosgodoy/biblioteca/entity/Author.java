287fd46df7b2dcacc0b034a65abef8780c32b0b6
Fabio <fabiomdg1@gmail.com>
2022-06-16 17:49:54 -0300

    configurado lombok

--- biblioteca/src/main/java/com/fabiomattosgodoy/biblioteca/entity/Author.java
+++ biblioteca/src/main/java/com/fabiomattosgodoy/biblioteca/entity/Author.java
@@ -1,8 +1,19 @@
 package com.fabiomattosgodoy.biblioteca.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+
 public class Author {
 
     @Id
